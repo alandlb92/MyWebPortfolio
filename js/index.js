8aaf95087ba526223bc2d50a42c523b41aef8464
Alan <alan.dlucas.bittencourt@gmail.com>
2026-04-16 17:23:57 -0300

    Adjust nav for mobile

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 var ProjectHtml;
 
+function adjustContentMargin() {
+    const h = document.querySelector("header").offsetHeight;
+    contentWrapper.style.marginTop = h + "px";
+}
 
 async function OnStartMainPage() {
     console.log("Main page loaded");
     const module = await import("./MainContent.js");
-    module.OnStart();    
+    module.OnStart();
 }
 
 
@@ -13,6 +17,8 @@ document.addEventListener("DOMContentLoaded", async function () {
         const response = await fetch("Pages/Modules/MainContent.html");
         const html = await response.text();
         content.innerHTML = html;
+        adjustContentMargin();
+        window.addEventListener("resize", adjustContentMargin);
         OnStartMainPage();
     } catch (error) {
         console.error("Erro ao carregar HTML:", error);
